9f22b53c2cc88e03c6202b6f5cb8f7ca8ca19335
dongchun <kobolt0@hanmail.net>
2022-08-26 17:17:57 +0900

    진행상황: rest 호출해서 결과 로그찍는데까지

--- src/main/java/kr/go/spo/workflow/MsgListener.java
+++ src/main/java/kr/go/spo/workflow/MsgListener.java
@@ -3,10 +3,18 @@ package kr.go.spo.workflow;
 import camundajar.impl.com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 
+import javax.jms.Session;
 import java.util.Map;
 
 @Slf4j
@@ -14,14 +22,42 @@ import java.util.Map;
 @Controller
 public class MsgListener {
 
+    @Value("${workflow.extrc.serverIp}")
+    String apiServerIp;
+
     @JmsListener(destination = "extrc")
-    public void receiveMessage(String msg) {
+    public void receiveMessage(@Payload String strMsg,
+                               @Headers MessageHeaders headers,
+                               Message message, Session session) {
+        log.info("- - - - - - - - - - - - - - - - - - - - - - - -");
+        log.info("######          Message Order           #####");
+        log.info("- - - - - - - - - - - - - - - - - - - - - - - -");
+        log.info("received <" + strMsg + ">");
+
+        log.info("- - - - - - - - - - - - - - - - - - - - - - - -");
+        log.info("######          Message Details           #####");
+        log.info("- - - - - - - - - - - - - - - - - - - - - - - -");
+        log.info("headers: " + headers);
+        log.info("message: " + message);
+        log.info("session: " + session);
+        log.info("- - - - - - - - - - - - - - - - - - - - - - - -");
+
 
 
         Gson gson = new Gson();
-        Map<String, Object> jsonMap = gson.fromJson(msg, Map.class);
-        log.info("##@# start process {}", msg);
+        Map<String, Object> jsonMap = gson.fromJson(strMsg, Map.class);
+        log.info("##@# start process {}", strMsg);
+        this.startProcess(jsonMap);
+    }
 
+
+    public ProcessInstance startProcess(Map<String, Object> variables) {
+        String processId = "extrc";
+        ProcessInstanceWithVariables instance = ProcessEngines.getDefaultProcessEngine().getRuntimeService().createProcessInstanceByKey(processId)
+                .setVariables(variables)
+                .executeWithVariablesInReturn()
+                ;
+        return instance;
     }
 
 }
